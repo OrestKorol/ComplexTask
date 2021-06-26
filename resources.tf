03bb7d14f605bb2cfd93de0ada44a20eeb011804
OrestKorol <85111861+OrestKorol@users.noreply.github.com>
2021-06-26 17:53:32 +0300

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -39,7 +39,7 @@ resource "aws_default_route_table" "complex_route_table" {
 
 resource "aws_key_pair" "ssh-key" {
 	key_name = "complex-key"
-	public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDyFO85Bz5tGehYachlOEaHQWUSLRmLcnq2BuX3KoCqmQtPN19BkaDARG2CW0xJHZkumdomz6P6Ihn6msUGNgQLn5MTJxalXWsrVvKgndB+RJg1rOJ4rdeszL/wzWa/1VbBr0dOI3lxuPCv9LbRV7ZgCGutNpuLygEwi6Vu18FIsxF7xotIazqbC5SMG/oDgZOR9m5u0PMFwb82JXIb/JqOezDd7uT1ZAQPQ75QBuHBjm48m5kL2j3SX6u8GUnCuQCIvG7dE77FoCaeyK7oJ7C51UI8vqpKuSsr5urFdGSv289EpjIP/cGyeAzA/yZ/Td7O14xGXvVF0ka/TFr9Kn8WHDC+F1addKIVMNj9NceBtAdEiCSdR44kC0D9pmIa6EOgJi25LZbxXUDqmEYzjxQeb+Cv/jMmgLVWO1bhOrEWIaFEa9yglOfUnWUdDIY3c5grTlxpL1Yd7cAC4Jc0DGXvGyo3//NOtFSqynlifS+r3P4uCFGbG1ygT/Z41V2zhrs= Orest Korol@ThatITGuy"
+	public_key = ""
 }
 
 resource "aws_instance" "sql_instance"{
