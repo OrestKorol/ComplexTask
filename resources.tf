60925060c46429d3cec865085932d1b5e5cb5717
OrestKorol <orest.i.korol.@gmail.com>
2021-06-28 14:38:07 +0300

    added security_group & fixed instances

--- resources.tf
+++ resources.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "complex_vpc" {
 	cidr_block = "10.0.0.0/16"
+	enable_dns_hostnames = true
 	
 	tags = {
 		Name = "complex_task"
@@ -42,6 +43,23 @@ resource "aws_route_table_association" "complex_association" {
 	route_table_id = aws_route_table.complex_route_table.id
 }
 
+resource "aws_security_group" "complex_security" {
+	name = "complex_security"
+	vpc_id = aws_vpc.complex_vpc.id
+	ingress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
 resource "aws_key_pair" "ssh-key" {
 	key_name = "complex-key"
 	public_key = ""
@@ -51,11 +69,12 @@ resource "aws_instance" "sql_instance"{
 	ami = "ami-0721c9af7b9b75114"
 	instance_type = "t2.micro"
 	key_name = "complex-key"
+	associate_public_ip_adress = true
 	subnet_id = aws_subnet.complex_subnet.id
+	vpc_security_group_ids = [aws_security_group.complex_security.id]
 	
-
 	tags = {
-		Name = "SQL_Instance"
+		Name = "SQLInstance"
 	}
 }
 
@@ -63,9 +82,11 @@ resource "aws_instance" "tomcat_instance"{
 	ami = "ami-0721c9af7b9b75114"
 	instance_type = "t2.micro"
 	key_name = "complex-key"
+	associate_public_ip_adress = true
 	subnet_id = aws_subnet.complex_subnet.id
-
+	vpc_security_group_ids = [aws_security_group.complex_security.id]
+	
 	tags = {
-		Name = "TomCat_Instance"
+		Name = "TomCatInstance"
 	}
 }
