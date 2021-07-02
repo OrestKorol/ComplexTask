f4cf0e8614773bc83d3d8020a79ea66311991505
OrestKorol <orest.i.korol.@gmail.com>
2021-07-02 16:13:57 +0300

    fixed script

--- configure_hosts.sh
+++ configure_hosts.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 ip=`terraform output sql_ip` ; echo -en "[mysql]\n $ip ansible_ssh_user=ec2-user" >> Ansible/hosts
 ip=`terraform output tomcat_ip` ; echo -en "\n[tomcat]\n $ip ansible_ssh_user=ec2-user" >> Ansible/hosts
