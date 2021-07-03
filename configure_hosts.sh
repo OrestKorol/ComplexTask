9308fa8cd478f208ed128f2627c896cf8a918ab5
OrestKorol <orest.i.korol.@gmail.com>
2021-07-03 14:10:31 +0300

    fixed ip adress writing

--- configure_hosts.sh
+++ configure_hosts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ip=`terraform output -raw sql_ip` ; echo -en "\n[mysql]\n$ip ansible_ssh_user=ec2-user" >> Ansible/hosts
-ip=`terraform output -raw tomcat_ip` ; echo -en "\n[tomcat]\n$ip ansible_ssh_user=ec2-user" >> Ansible/hosts
+ip=`terraform output -raw sql_ip` ; echo -en "\n[mysql]\n$ip" >> Ansible/hosts
+ip=`terraform output -raw tomcat_ip` ; echo -en "\n[tomcat]\n$ip" >> Ansible/hosts
