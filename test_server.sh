75074cfe992272a9316a85b53371eefd0bf348d8
OrestKorol <orest.i.korol.@gmail.com>
2021-07-05 17:18:06 +0300

    testing if tomcat is alive

---
+++ test_server.sh
@@ -0,0 +1,6 @@
+ip='terraform output -raw tomcat_ip'
+ip=$(terraform output -raw tomcat_ip)
+port=":8080/SampleWebApp/"
+adress="${ip}${port}"
+echo "${adress}"
+wget --spider --server-response "${address}" 2>&1 | grep '200\ OK' | wc -l
